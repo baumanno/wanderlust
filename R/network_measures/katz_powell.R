2055671e8bbf033e9ff06bbec3d41415eeaacce1
Oliver Baumann <oliver.baumann@mailbox.org>
2018-10-12 14:19:36 +0200

    Include custom network measures
    These measures are not implemented by the igraph package.
    
    katz_powell.R
    -------------
    For a given network, compute the Katz-Powell-index of mutuality. See
    Wassermann/Faust
    
    krackhardt.R
    ------------
    For a given network, topic and node, compute the Krackhardt-Stern E-I
    index, indicating homophily or heterophily.
    
    weighted_mutuality.R
    --------------------
    Custom measure, computes a normalised difference of incoming and
    outgoing edges, thus indicating a form of mutuality.

---
+++ R/network_measures/katz_powell.R
@@ -0,0 +1,26 @@
+library(igraph)
+
+#' Compute the Katz-Powell-Index of mutuality
+#'
+#' See [Wassermann p.514]; the index allows comparisons of mutuality across
+#' varying network sizes. This is a global measure for the entire network.
+#' However, since we consider only ego->alter and alter->ego, but not
+#' alter->alter ties, this can be viewed as a measure describing the structure
+#' of ties for a single ego
+#'
+#' @param graph An igraph object.
+#'
+#' @return The Katz-Powell index value
+#' @export
+#'
+katz_powell_mutuality <- function(graph) {
+  if (vcount(graph) == 0)
+    NA
+  
+  L <- ecount(graph)
+  L2 <- sum(degree(graph, mode = "out") ** 2)
+  g <- vcount(graph)
+  M <- dyad_census(graph)$mut
+  
+  (2 * (g - 1) ** 2 * M - L ** 2 + L2) / (L * (g - 1) ** 2 - L ** 2 + L2)
+}
---
+++ R/network_measures/weighted_mutuality.R
@@ -0,0 +1,47 @@
+library(igraph)
+
+#' Compute weighted mutuality (better word?)
+#'
+#' For a given node V, compute the normalized difference of incoming
+#' and outgoing edges.
+#' This measure varies in [-1;1], with 0 denoting the balanced case where
+#' |in| = |out|
+#' A source node will exhibit a value close to -1, a sink node close to 1.
+#'
+#' @param graph An igraph graph object.
+#' @param node A string. The vertex-name to compute the index for.
+#'
+#' @return A numeric value
+#' @export
+#'
+#' @examples
+weighted_mutuality <- function(graph, node, debug = FALSE) {
+  if (vcount(graph) == 0)
+    return(NA)
+  
+  if (debug)
+    print(graph)
+  
+  # outdegree d+
+  out <- length(incident(graph, node, mode = "out"))
+  
+  if (debug)
+    message(paste("outgoing: ", out))
+  
+  # indegree d-
+  inc <- length(incident(graph, node, mode = "in"))
+  
+  if (debug)
+    message(paste("incoming: ", inc))
+  
+  total <- inc + out
+  
+  # no mutuality for isolated nodes
+  if (total == 0)
+    return(NA)
+  
+  if (debug)
+    print((inc - out) / total)
+  
+  (inc - out) / total
+}

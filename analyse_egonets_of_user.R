afafd00b9f354f0107126b58f29a354e1fda2bc8
Oliver Baumann <oliver.baumann@mailbox.org>
2018-10-15 23:33:32 +0200

    Use new read-methods for fetching data
    Integrate the new read-methods for fetching the data for analysis,
    aggregate it and analyse it based on the new structure of the data.
    Add markers.

--- analyse_egonets_of_user.R
+++ analyse_egonets_of_user.R
@@ -1,8 +1,16 @@
+
+
+
+# 0: Libraries ------------------------------------------------------------
+
 library(tidyverse)
 library(glue)
 library(gridExtra)
 library(igraph)
 library(lubridate)
+
+
+# 1: External functions ---------------------------------------------------
 
 source("R/reading_data/read_snapshots.R")
 source("R/network_construction/build_from_edgelist.R")
@@ -10,34 +18,56 @@ source("R/network_measures/katz_powell.R")
 source("R/network_measures/weighted_mutuality.R")
 source("R/network_measures/krackhardt.R")
 
-user <- c("monocasa")#, "formido", "cavedave", "IronWolve")
+
+# 2: global constants -----------------------------------------------------
+
+#user <- c("monocasa", "formido", "cavedave", "IronWolve")
+user <- "IronWolve"
+
+# 3: Read data from snapshots ---------------------------------------------
 
 edgelists <- read_edgelist(user)
+topics_of_alters <- read_alters_topics(user)
 
-topic_metadata_df <- read_alters_topics(user)
+# the ego is missing from the alters snapshots, so we add it manually with a
+# special topic of -1
 
-topic_metadata_df <- read_monthly_snapshot_data(user, path = "data/{user}/alters-topics-{year}-{month}.csv")
+fix_missing_ego <- function(df) {
+  if (nrow(df) == 0) {
+    return(df)
+  }
+  add_row(df, author = user, topic = -1)
+}
 
-topic_metadata_df <- lapply(topic_metadata_df, function(x) {
-  x %>% 
-    select(-subreddit, -maxcount, -year, -month) %>% 
-    add_row(author = user, topic = -1)
-})
+topics_of_alters <-
+  topics_of_alters %>%
+  mutate(data = map(data, fix_missing_ego))
 
-# ... and construct a network from it
-list_of_graphs <-
-  build_egonet_from_edgelist(edgelists, topic_metadata_df)
 
-save(list_of_graphs, file = "output/list_of_graphs.rda")
+# 4: aggregate data into single dataframe ---------------------------------
 
-graph_store <-
-  transmute(
-    month,
-    year,
-    author = user,
-    vertices = sapply(list_of_graphs, vcount),
-    katz_powell = sapply(list_of_graphs, katz_powell_mutuality),
-    weighted = mapply(weighted_mutuality, list_of_graphs, author)
+# store edgelists and topic metadata in a frame
+df <-
+  left_join(edgelists, topics_of_alters, by = c("month", "year", "author")) %>%
+  select(year,
+         month,
+         author,
+         edgelist = data.x,
+         alters_topics = data.y)
+
+# remove the now unused objects
+rm(edgelists, topics_of_alters)
+
+# 5: analysis of egonetwork -----------------------------------------------
+
+df <- df %>%
+  mutate(graph = map2(edgelist, alters_topics, egonet_from_edgelist))
+
+graph_analysis <- df %>%
+  mutate(
+    vertices = map_int(graph, vcount),
+    katz_powell = map_dbl(graph, katz_powell_mutuality),
+    weighted = map_dbl(graph, weighted_mutuality, node = author)
   )
 
 save(graph_store, file = "output/graph_store.rda")

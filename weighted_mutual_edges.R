ab8cc7a4b5cfc6d731db779a0414063918097c6c
Oliver Baumann <oliver.baumann@mailbox.org>
2018-10-25 15:51:45 +0200

    Construct the dataframe once, remove old plots
    Constructing the dataframe, including joins and mutations, once at the
    beginning of the analysis makes subsequent code a lot more readable.

--- weighted_mutual_edges.R
+++ weighted_mutual_edges.R
@@ -37,16 +37,17 @@ load(glue("output/{USERNAME}-graph_analysis.rda"))
 # all snapshots for one ego
 ego_topics <- read_ego_topics(USERNAME)
 
-# construct subgraph and statistics
-graphs_and_topics <-
-  graph_data %>% 
+# construct all required data from graphs and proportions
+df <-
+  graph_data %>%
   left_join(ego_topics, by = c("year", "month", "author")) %>%
-  rename(ego_topics = data) %>% 
-  drop_na() %>% 
+  rename(ego_topics = data) %>%
+  drop_na() %>%
+  left_join(corr_df, by = c("year", "month")) %>%
   mutate(
     date = make_date(year, month),
     topic_subgraph = map2(graph, ego_topics, filter_graph_wrapper),
-    topical_overlap = map2_dbl(topical_subgraph, graph, function(a, b) {
+    topical_overlap = map2_dbl(topic_subgraph, graph, function(a, b) {
       # compute ratio of edges of two graphs
       
       if (ecount(a) == 0 | ecount(b) == 0) {
@@ -54,25 +55,24 @@ graphs_and_topics <-
       }
       
       ecount(a) / ecount(b)
-    })
-  ) %>% 
-  drop_na()
+    }),
+    ratio_edges = map_dbl(topic_subgraph, directed_edge_ratio, user = USERNAME),
+    weighted_ratio = ratio_edges * prop_ego
+  ) %>%
+  drop_na() %>%
+  select(date,
+         topic,
+         topical_overlap,
+         ratio_edges,
+         prop_ego,
+         prop_alters,
+         weighted_ratio)
 
 # 6: plot data ------------------------------------------------------------
 
-# graph statistics and topic distributions
-graphs_and_proportions <- corr_df %>%
-  left_join(graphs_and_topics, by = c("year", "month")) %>%
-  select(date,
-         topic,
-         prop_ego,
-         prop_alters,
-         topical_overlap,
-         relative_topic_reciprocity)
-
-graphs_and_proportions %>% 
-  group_by(topic) %>% 
-  filter(n() > MIN_OBS) %>% 
+df %>%
+  group_by(topic) %>%
+  filter(n() > MIN_OBS) %>%
   ggplot(mapping = aes(prop_ego, topical_overlap, colour = topic)) +
   geom_point(alpha = P_ALPHA) +
   geom_smooth(method = "lm") +
@@ -83,58 +83,33 @@ graphs_and_proportions %>%
     title = "Proportion of topical edges in graph vs prop. of posts to a topic",
     caption = glue("Minimum number of observations: {MIN_OBS}")
   ) +
-  facet_wrap(~topic)
+  facet_wrap( ~ topic)
 
 ggsave(filename = glue("figs/{USERNAME}-WEIGHTS-prop-vs-overlap.png"))
 
-graphs_and_proportions %>%
-  group_by(topic) %>% 
-  filter(n() > MIN_OBS) %>% 
-  ggplot(mapping = aes(prop_ego, relative_topic_reciprocity, colour = topic)) +
-  geom_point(alpha = P_ALPHA) +
-  geom_smooth() +
-  #scale_y_continuous(limits = c(-1, 1)) +
-  labs(
-    x = "prop. of Ego's posts",
-    y = "relative reciprocity of subgraph",
-    title = "Difference of in/out edges relative to all edges, vs. prop of posts",
-    caption = glue("Minimum number of observations: {MIN_OBS}")
-  ) +
-  facet_wrap(~topic)
-
-ggsave(filename = glue("figs/{USERNAME}-WEIGHTS-prop-vs-relative-reciprocity.png"))
-
-weight_mutual_edges <-
-  graphs_and_topics %>%
-  left_join(corr_df, by = c("year", "month")) %>% 
+df %>%
   group_by(topic) %>%
-  mutate(
-    t = prop_ego * relative_topic_reciprocity
-  ) %>% 
-  ungroup()
-
-weight_mutual_edges %>%
-  group_by(topic) %>% 
-  filter(n() > MIN_OBS) %>% 
+  filter(n() > MIN_OBS) %>%
   ggplot(mapping = aes(date, weighted_ratio, colour = topic)) +
   geom_point(alpha = P_ALPHA) +
   geom_smooth(method = "lm") +
-  facet_wrap( ~ topic) +
+  facet_wrap(~ topic) +
   labs(
     x = "",
     y = "weighted reciprocity",
     title = "Weight edge difference by proportion of posts",
     caption = glue("Minimum number of observations: {MIN_OBS}")
   )
+
 # new
-weight_mutual_edges %>%
-  group_by(topic) %>% 
-  filter(n() > MIN_OBS) %>% 
-  ggplot(mapping = aes(prop_ego, t, colour = topic)) +
+df %>%
+  group_by(topic) %>%
+  filter(n() > MIN_OBS) %>%
+  ggplot(mapping = aes(prop_ego, weighted_ratio, colour = topic)) +
   geom_point(alpha = P_ALPHA) +
   geom_smooth(method = "lm") +
   geom_abline() +
-  facet_wrap( ~ topic) +
+  facet_wrap(~ topic) +
   labs(
     x = "prop ego",
     y = "weighted reciprocity",
@@ -142,23 +117,9 @@ weight_mutual_edges %>%
     caption = glue("Minimum number of observations: {MIN_OBS}")
   )
 
-ggsave(filename = glue("figs/{USERNAME}-WEIGHTS-weighted-relative-reciprocity.png"))
-
-weight_mutual_edges %>% 
-  group_by(topic) %>% 
-  filter(n() > MIN_OBS) %>% 
-  ggplot(mapping = aes(t)) +
-  geom_point(mapping = aes(y = prop_ego, colour = "Ego"), alpha = P_ALPHA) +
-  #geom_smooth(mapping = aes(y = prop_ego, colour = "Ego")) +
-  geom_point(mapping = aes(y = prop_alters, colour = "Alters"), alpha = P_ALPHA) +
-  #geom_smooth(mapping = aes(y = prop_ego, colour = "Alters")) +
-  facet_wrap(~topic) +
-  labs(
-    x = "probability-weighted reciprocity",
-    y = "proportion of posts by ego"
-  )
-
-ggsave(filename = glue("figs/{USERNAME}-WEIGHTS-prop-vs-weighted-reciprocity.png"))
+ggsave(filename = glue(
+  "figs/{USERNAME}-WEIGHTS-weighted-relative-reciprocity.png"
+))
 
 graphs_and_topics %>%
   mutate(cs = cumsum(topical_overlap)) %>%
@@ -169,4 +130,3 @@ graphs_and_topics %>%
        title = "Cumsum of ratio of topical edges")
 
 ggsave(filename = glue("figs/{USERNAME}-WEIGHTS-cumsum-topical-overlap.png"))
-

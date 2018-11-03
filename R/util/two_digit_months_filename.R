9a2ca8a4d43a9a90950681dd3a432c96ffc3c8f3
Oliver Baumann <oliver.baumann@mailbox.org>
2018-11-03 22:17:44 +0100

    Fix bug in month-regex

--- R/util/two_digit_months_filename.R
+++ R/util/two_digit_months_filename.R
@@ -10,5 +10,5 @@ mclapply(users, function(user) {
   path <- glue("data/{user}")  
   
   files <- dir_ls(path = path, regexp = "\\d{4}-\\d{1,2}")
-  file_move(files, str_replace(files, "-(\\d{1}\\.", "-0\\1\\."))
+  file_move(files, str_replace(files, "-(\\d{1})\\.", "-0\\1\\."))
 })

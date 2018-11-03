3499abdfb5e60094f1abf1e59f8c0c97243a3874
Oliver Baumann <oliver.baumann@mailbox.org>
2018-11-03 21:26:37 +0100

    Parallelize fixing of month-digits
    Since we need to fix all filenames evenutally, the script executes
    renaming in parallel over all users. Also, the renaming can be handled
    more efficiently by just string-replacing the occurence of one digit
    with a 0-prefixed version.

--- R/util/two_digit_months_filename.R
+++ R/util/two_digit_months_filename.R
@@ -2,18 +2,13 @@ library("fs")
 library("glue")
 library("stringr")
 library("purrr")
+library("parallel")
 
-user <- readline("Please enter a username: ")
-path <- glue("data/{user}")
+users <- c("monocasa", "formido", "cavedave", "IronWolve")
 
-(files <- dir_ls(path = path, regexp = "ego-*"))
-
-walk(files, function(f) {
-  d <- str_extract(f, "\\d{4}-\\d{1,2}")
-  d <- c(str_split(d, "-")[[1]])
-  d <- make_date(d[1], d[2])
-  new <- strftime(d, "%Y-%m")
-  file.rename(f, glue("{path}/ego-topics-{new}.csv"))
+mclapply(users, function(user) {
+  path <- glue("data/{user}")  
+  
+  files <- dir_ls(path = path, regexp = "\\d{4}-\\d{1,2}")
+  file_move(files, str_replace(files, "-(\\d{1}\\.", "-0\\1\\."))
 })
-
-system(glue("ls {path}"))

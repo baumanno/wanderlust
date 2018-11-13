b23efecfc014a038a566492631c90951bc2f1669
Oliver Baumann <oliver.baumann@mailbox.org>
2018-11-13 19:39:56 +0100

    Return empty graph when no nodes left
    If after filtering there are no more nodes in the graph, return an empty
    graph instead of NA. This not only leads to a correct type signature of
    this function, it also makes handling output in a pipeline more straight
    forward.

--- R/network_construction/filter_graph.R
+++ R/network_construction/filter_graph.R
@@ -5,17 +5,16 @@ library(igraph)
 #' Filters the nodes in the given graph to only return
 #' those matching the topic number, returning the subgraph containing these
 #' nodes.
-#' 
-#' @param graph 
-#' @param topic_number 
+#'
+#' @param graph
+#' @param topic_number
 #'
 #' @return An igraph graph
-filter_graph <- function (g, topic_numbers) {
-  
-  if(length(V(g)) == 0) {
+filter_graph <- function(g, topic_numbers) {
+  if (length(V(g)) == 0) {
     return(make_empty_graph())
   }
-  
+
   # We must remove nodes that don't have a topic assigned,
   # as these will cause problems when iterating over the graph.
   g <- g - V(g)[is.na(topic)]
@@ -25,7 +24,7 @@ filter_graph <- function (g, topic_numbers) {
   vs <- V(g)[topic %in% topic_numbers | topic == -1]
   
   if (length(vs) == 0) {
-    return(NA)
+    return(make_empty_graph())
   }
   
   induced_subgraph(g, vs)

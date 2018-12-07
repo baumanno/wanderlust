2ce80fe31f8432b43650c1dbb83e919aae198203
Oliver Baumann <oliver.baumann@mailbox.org>
2018-12-07 06:10:07 +0100

    Checkpoint

--- R/topic_distributions/plot.R
+++ R/topic_distributions/plot.R
@@ -8,7 +8,7 @@ area_chart <- function(df) {
     geom_area(
       size = S_SIZE,
       colour = S_COLOR,
-      alpha = F_ALPHA,
+      alpha = .9,
       position = "stack"
     ) +
     labs(x = "date",
@@ -24,10 +24,10 @@ fill_colours <- function(topics) {
   n_topics <- length(topics)
   
   # Brewer-palette suitable for qualitative data
-  pal <- brewer.pal(9, "Set3")
+  pal <- brewer.pal(9, "Paired")
   
   # inflate palette by interpolating to `num_topics` colors
-  cols <- colorRampPalette(pal)(n_topics)
+  cols <- colorRampPalette(pal, bias = .75)(n_topics)
   
   # must be named list to index properly
   names(cols) <- topics
